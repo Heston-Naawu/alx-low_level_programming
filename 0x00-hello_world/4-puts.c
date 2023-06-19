ae0544a9179772d5e118a2e7672e185da9fbb43f
HestonNaawu <contact.heston@gmail.com>
2023-06-19 00:51:54 +0000

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
