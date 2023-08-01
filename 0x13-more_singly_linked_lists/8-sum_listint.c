9215879707f07a96a3159d482413b187e85b43d4
Heston-Naawu <contact.heston@gmail.com>
2023-08-01 06:10:40 +0000

    Task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,10 +1,4 @@
-/*
- * File: 8-sum_listint.c
- * Auth: Gedeon Obae Gekonge
- */
-
 #include "lists.h"
-
 /**
  * sum_listint - Calculates the sum of all the
  *               data (n) of a listint_t list.
