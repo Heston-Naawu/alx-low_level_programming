1f168621ba2bbfa0fe1aec64a96a0d3d735bebc9
Heston-Naawu <contact.heston@gmail.com>
2023-08-01 06:17:15 +0000

    Task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,11 +1,5 @@
-/*
- * File: 101-print_listint_safe.c
- * Auth: Gedeon Obae Gekonge
- */
-
 #include "lists.h"
 #include <stdio.h>
-
 size_t looped_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 
