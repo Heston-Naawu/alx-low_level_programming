72751733c80d4baf6a59e974e17d366fb0e80db4
HestonNaawu <contact.heston@gmail.com>
2023-10-27 03:59:40 +0000

    Main Added

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef _BIT_MANIP_
 #define _BIT_MANIP_
 
+
 int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
@@ -12,4 +13,5 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void)
 
+  
 #endif
