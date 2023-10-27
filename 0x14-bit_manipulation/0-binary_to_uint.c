a192fc5d09db0a734fcf35393ab3c629cd47a103
HestonNaawu <contact.heston@gmail.com>
2023-10-27 03:57:38 +0000

    Task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,25 +10,19 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int len = 0, count = 0, sum = 0;
-
 	if (b == NULL)
 		return (0);
-
 	len = _strlen(b);
 	while (len--)
 	{
 		if (b[len] != 48 && b[len] != 49)
 			return (0);
-
 		if (b[len] == 49)
 			sum += 1 << count;
-
 		count++;
 	}
-
 	return (sum);
 }
-
 /**
   * _strlen - Returns the length of a string
   * @s: String to count
@@ -38,9 +32,7 @@ unsigned int binary_to_uint(const char *b)
 int _strlen(const char *s)
 {
 	int c = 0;
-
 	while (s[c])
 		c++;
-
 	return (c);
 }
